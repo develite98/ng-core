d997331c267b9ad9701da876c24d6521f0007fc0
Phong Cao <43846216+phongcao3091998@users.noreply.github.com>
2021-06-14 09:14:44 +0700

    feat(core): feat(core): feat(core): feat(core): feat(core): feat(core): add release sh (#7)
    Co-authored-by: phong3091998 <phong.cao@orientsoftware.com>

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -20,7 +20,7 @@ RELEASE_TYPE=${1:-$(getBuildType "$COMMIT_MESSAGE")}
 DRY_RUN=${DRY_RUN:-"False"}
 
 
-AFFECTED=$(node node_modules/.bin/nx affected:libs --plain --base=origin/main~1)
+AFFECTED=$(node node_modules/.bin/nx affected:libs --plain --base=origin/main)
 if [ "$AFFECTED" != "" ]; then
   cd "$PARENT_DIR"
   echo "Copy Environment Files"
